98532c47f90cf2f7039be5d993b4784c76c556dc
homeHaraMBP <bizhara@gmail.com>
2020-07-25 20:34:37 +0900

    add mock variation

--- TryCollevtionView/TryCollevtionView/MockCellData.swift
+++ TryCollevtionView/TryCollevtionView/MockCellData.swift
@@ -9,14 +9,14 @@
 struct MockCellData {
     static let mockCellData = [
         CollectionViewCellData(title: "1: セルタイトル", detail: "説明テキスト"),
-        CollectionViewCellData(title: "2: 長めのセルタイトルにすると表示はどうなるだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
+        CollectionViewCellData(title: "2: 1行に入り切らないセルタイトルにすると末尾が省略表示になる設定だがどうだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
         CollectionViewCellData(title: "3: セルタイトル", detail: "説明テキスト"),
-        CollectionViewCellData(title: "4: 長めのセルタイトルにすると表示はどうなるだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
+        CollectionViewCellData(title: "4: 1行に入り切らないセルタイトルにすると末尾が省略表示になる設定だがどうだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
         CollectionViewCellData(title: "5: セルタイトル", detail: "説明テキスト"),
-        CollectionViewCellData(title: "6: 長めのセルタイトルにすると表示はどうなるだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
+        CollectionViewCellData(title: "6: 1行に入り切らないセルタイトルにすると末尾が省略表示になる設定だがどうだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
         CollectionViewCellData(title: "7: セルタイトル", detail: "説明テキスト"),
-        CollectionViewCellData(title: "8: 長めのセルタイトルにすると表示はどうなるだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
+        CollectionViewCellData(title: "8: 1行に入り切らないセルタイトルにすると末尾が省略表示になる設定だがどうだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？"),
         CollectionViewCellData(title: "9: セルタイトル", detail: "説明テキスト"),
-        CollectionViewCellData(title: "10: 長めのセルタイトルにすると表示はどうなるだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？")
+        CollectionViewCellData(title: "10: 1行に入り切らないセルタイトルにすると末尾が省略表示になる設定だがどうだろうか？", detail: "説明は複数行の設定にしてあるので、全部表示されるのが正解なのだが、どうだろうか？")
     ]
 }
